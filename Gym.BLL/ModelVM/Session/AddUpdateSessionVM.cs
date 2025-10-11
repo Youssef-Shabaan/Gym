236699505ef6f9d19a8a2d515378050eb4061227
Youssef-Shabaan <youssefshabaan305@gmail.com>
2025-10-11 14:56:53 +0300

    Add Session Service

---
+++ Gym.BLL/ModelVM/Session/AddUpdateSessionVM.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Gym.BLL.ModelVM.Session
+{
+    public class AddUpdateSessionVM
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get;  set; }
+
+        public string? Description { get;  set; }
+
+        [Required(ErrorMessage = "Schedule Time is required")]
+        public DateTime ScheduleTime { get;  set; }
+
+        [Required(ErrorMessage = "Capacity is required")]
+        public int Capacity { get;  set; }
+    }
+}
