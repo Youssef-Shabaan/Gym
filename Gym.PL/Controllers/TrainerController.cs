5ae917b43e77de0cee5ba63fc3b8629ead1f177d
HusseinHashiem <husshashiem@gmail.com>
2025-11-27 13:11:16 +0200

    trainers and sessions views

---
+++ Gym.PL/Controllers/TrainerController.cs
@@ -0,0 +1,19 @@
+﻿using Gym.BLL.Service.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gym.PL.Controllers
+{
+    public class TrainerController : Controller
+    {
+        private readonly ITrainerService _trainerService;
+        public TrainerController(ITrainerService trainerService)
+        {
+            _trainerService = trainerService;
+        }   
+        public IActionResult Index()
+        {
+            var result = _trainerService.GetAll();
+            return View(result.Item3);
+        }
+    }
+}
